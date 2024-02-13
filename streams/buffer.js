9db738e363482b1218169876086eb4088e33b0c5
lukeskw <lucasporfirioa@gmail.com>
2024-02-13 20:48:36 -0300

    understanding buffers

---
+++ streams/buffer.js
@@ -0,0 +1,5 @@
+// represents a space on the computer's memory, used to transit data quickly
+// *check typedArray / native js way to deal with binary
+
+const buf = Buffer.from('hello world') // <- hexadecimal representation of each character
+console.log(buf)
