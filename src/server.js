60e9b19cd88fd3a071b1c4ab24d30ac37ecfffc9
lukeskw <lucasporfirioa@gmail.com>
2024-02-13 16:37:27 -0300

    finishing second content from first module

--- src/server.js
+++ src/server.js
@@ -5,13 +5,19 @@ import http from 'node:http'
 
 // stateful - stateless -> save data on memory / do not save data on memory
 
+// headers(on req and res) => metadata of each request
+
+//http status codes => 200, 201, 400, 404, 500
+
 const users = []
 
 const server = http.createServer((request, response)=>{
   const { method, url } = request
 
   if(method === 'GET' && url === '/users'){
-    return response.end(JSON.stringify(users))
+    return response
+    .setHeader('Content-type', 'application/json')
+    .end(JSON.stringify(users))
   }
   if(method === 'POST' && url === '/users'){
     users.push({
@@ -19,8 +25,9 @@ const server = http.createServer((request, response)=>{
       name: 'John doe',
       email: 'johndoe@example.com'
     })
+    return response.writeHead(201).end('ok')
   }
-  return response.end('ok')
+  return response.writeHead(404).end()
 })
 
 server.listen(3333)
