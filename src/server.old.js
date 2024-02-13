a9847bdaec592bd92073b9d0a6b6e5dc700d262c
lukeskw <lucasporfirioa@gmail.com>
2024-02-13 20:42:02 -0300

    understanding how node deals with body of reqs

--- src/server.old.js
+++ src/server.old.js
@@ -11,20 +11,8 @@ import http from 'node:http'
 
 const users = []
 
-const server = http.createServer(async (request, response)=>{
+const server = http.createServer((request, response)=>{
   const { method, url } = request
-
-  const buffers = []
-
-  for await (const chunk of request){
-    buffers.push(chunk)
-  }
-
-  try{
-    request.body = JSON.parse(Buffer.concat(buffers).toString())
-  } catch {
-    request.body = null
-  }
 
   if(method === 'GET' && url === '/users'){
     return response
@@ -32,13 +20,10 @@ const server = http.createServer(async (request, response)=>{
     .end(JSON.stringify(users))
   }
   if(method === 'POST' && url === '/users'){
-
-    const { name, email } = request.body
-
     users.push({
       id: 1,
-      name,
-      email,
+      name: 'John doe',
+      email: 'johndoe@example.com'
     })
     return response.writeHead(201).end('ok')
   }
